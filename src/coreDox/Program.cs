749588e7584595d6d1c6e9bea483b0f262676c54
Gerrit 'Geaz' Gazic <Geaz@users.noreply.github.com>
2017-01-13 22:10:24 +0100

    Added commonmark.net

--- src/coreDox/Program.cs
+++ src/coreDox/Program.cs
@@ -1,9 +1,12 @@
 using System;
+using CommonMark;
 
 class Program
 {
     static void Main(string[] args)
     {
+        var result = CommonMarkConverter.Convert("**Hello world!**");
+        
         Console.WriteLine("Hello World!");
     }
 }
