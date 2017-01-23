9069007f5cd9398635dc3bb9d20c65d3dea57f82
Gerrit 'Geaz' Gazic <Geaz@users.noreply.github.com>
2017-01-23 06:50:38 +0100

    Fixes because of restructure

--- src/[Plugs]/coreDox.Plugs.Wizard/Wizard.cs
+++ src/[Plugs]/coreDox.Plugs.Wizard/Wizard.cs
@@ -7,7 +7,7 @@ using coreDox.Core.Contracts;
 
 namespace coreDox.Plugs
 {
-    internal class Wizard : IPlug
+    public class Wizard : IPlug
     {
         public void Execute(List<PackageFile> packageFiles)
         {
